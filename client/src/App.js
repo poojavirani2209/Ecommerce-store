dd739d04f5657d4ba1e4d1c9dda629a001c6cc39
pooja virani <pooja1522@gmail.com>
2024-10-04 10:33:16 +0530

    Implement Dummy Login
    - Add 3 users and 1 admin userIds to accordingly show admin summary page
      or buy page
    - There is a hardcoded validation where userId must be either
      user1,user2,user3, or admin
    - set Login error otherwise

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,62 @@ import AdminOrderSummary from "./components/AdminOrderSummary";
 function App() {
   const [userId, setUserId] = useState("1"); // Hardcoded user ID for now
   const [refreshCart, setRefreshCart] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loginError, setLoginError] = useState("");
 
   const handleItemAddedToCart = () => {
     setRefreshCart(true);
   };
 
+  const handleLogin = () => {
+    if (
+      userId === "user1" ||
+      userId === "user2" ||
+      userId === "user3" ||
+      userId == "admin"
+    ) {
+      /**
+       * This is a dummy login page. Ideally should be handled by sign up and
+       * login by authenticating through backend.
+       */
+      setLoggedIn(true);
+      setLoginError("");
+    } else {
+      setLoginError("Invalid user ID");
+    }
+  };
+
+  if (!loggedIn) {
+    return (
+      <div className="Login">
+        <h2>Login</h2>
+        {loginError && <p style={{ color: "red" }}>{loginError}</p>}
+        <div>
+          <input
+            type="text"
+            placeholder="User ID"
+            value={userId || ""}
+            onChange={(e) => setUserId(e.target.value)}
+          />
+        </div>
+        <button onClick={handleLogin}>Login</button>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <ItemList userId={userId} onItemAddedToCart={handleItemAddedToCart} />
-      <Cart
-        userId={userId}
-        refreshCart={refreshCart}
-        setRefreshCart={setRefreshCart}
-      />
-      <AdminOrderSummary />
+      {userId == "admin" && <AdminOrderSummary />}
+      {userId != "admin" && (
+        <>
+          <ItemList userId={userId} onItemAddedToCart={handleItemAddedToCart} />
+          <Cart
+            userId={userId}
+            refreshCart={refreshCart}
+            setRefreshCart={setRefreshCart}
+          />
+        </>
+      )}
     </div>
   );
 }
